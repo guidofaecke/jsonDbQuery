4882c3004345e116e7c90cc67ef1f4e72972fb0e
Guido Faecke <guido_faecke@swifttrans.com>
2018-05-10 09:35:51 -0700

    Adding more operators

--- lib/JsonDbQuery/JsonDbQueryDoctrineAdapter.php
+++ lib/JsonDbQuery/JsonDbQueryDoctrineAdapter.php
@@ -64,32 +64,45 @@ class JsonDbQueryDoctrineAdapter extends JsonDbQueryCommon implements JsonDbQuer
      */
     public function generate()
     {
-//         Assertion::notNull($this->tableName);
+        Assertion::notNull($this->tableName);
 
         $query = json_decode($this->jsonQueryString, true);
 
         $key = key($query);
+
         if (array_search($key, $this->logicOperators)) {
             switch ($key) {
                 case '$or':
                     $criteria = $this->buildOr(current($query));
                     break;
+                case '$and':
+                    $criteria = $this->buildAnd(current($query));
+                    break;
             }
         }
 
+        if (! array_search($key, $this->logicOperators)) {
+            $operator = key($query[$key]);
+
+            if (array_search($operator, $this->conditionalOperators) !== false) {
+                switch ($operator) {
+                    case '$gt':
+                        $criteria = $this->buildGreaterThan($key, $query[$key][key($query[$key])]);
+                    case '$gte':
+                        $criteria = $this->buildGreaterThanEqual($key, $query[$key][key($query[$key])]);
+                    case '$lt':
+                        $criteria = $this->buildLessThan($key, $query[$key][key($query[$key])]);
+                    case '$lte':
+                        $criteria = $this->buildLessThanEqual($key, $query[$key][key($query[$key])]);
+                }
+            }
+
+        }
 
         $this->query = $this->entityManager->createQueryBuilder();
         $this->query->select('thing');
         $this->query->from($this->tableName, 'thing');
         $this->query->addCriteria($criteria);
-
-        var_dump($this->query->getQuery()->getSQL());
-        die();
-//             'select t1 from JsonDbQuery\Tests\Models\EntityFixture t1'
-//         );
-
-
-//         return $this->query->getSQL();
     }
 
     private function buildSql($item, $key)
@@ -103,11 +116,68 @@ class JsonDbQueryDoctrineAdapter extends JsonDbQueryCommon implements JsonDbQuer
     {
         $criteria = new Criteria();
 
-        foreach ($query as $field => $value) {
+        foreach (array_values($query) as $value) {
             $criteria->orWhere(
-                $criteria->expr()->eq($field, $value)
+                $criteria->expr()->eq(key($value), $value[key($value)])
             );
         }
+
+        return $criteria;
+    }
+
+    private function buildAnd(array $query)
+    {
+        $criteria = new Criteria();
+
+        foreach (array_values($query) as $value) {
+            $criteria->andWhere(
+                $criteria->expr()->eq(key($value), $value[key($value)])
+            );
+        }
+
+        return $criteria;
+    }
+
+    private function buildGreaterThan($field, $value)
+    {
+        $criteria = new Criteria();
+
+        $criteria->where(
+            $criteria->expr()->gt($field, $value)
+        );
+
+        return $criteria;
+    }
+
+    private function buildGreaterThanEqual($field, $value)
+    {
+        $criteria = new Criteria();
+
+        $criteria->where(
+            $criteria->expr()->gte($field, $value)
+        );
+
+        return $criteria;
+    }
+
+    private function buildLessThan($field, $value)
+    {
+        $criteria = new Criteria();
+
+        $criteria->where(
+            $criteria->expr()->lt($field, $value)
+            );
+
+        return $criteria;
+    }
+
+    private function buildLessThanEqual($field, $value)
+    {
+        $criteria = new Criteria();
+
+        $criteria->where(
+            $criteria->expr()->lte($field, $value)
+        );
 
         return $criteria;
     }
